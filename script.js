dcf8de38b7128a5c6a7dff40181d080f4297439c
Aleksandr Kozlov <kaa_12@mail.ru>
2020-02-15 13:34:16 +0800

    Add .editorconfig

--- script.js
+++ script.js
@@ -3,116 +3,116 @@ const studentInput = studentForm.querySelector('input#student');
 const studentListElement = document.querySelector('.student-list');
 
 const deleteButtonClickHandler = function (evt) {
-    const studentElement = evt.target.parentNode;
-    studentElement.classList.add('student_deleted');
-    setTimeout(function(){
-        if(studentElement.parentNode) {
-            studentListElement.removeChild(studentElement);
-        }
-    }, 500);
+  const studentElement = evt.target.parentNode;
+  studentElement.classList.add('student_deleted');
+  setTimeout(function () {
+    if (studentElement.parentNode) {
+      studentListElement.removeChild(studentElement);
+    }
+  }, 500);
 };
 
 const addDeleteButtonClickHandlerToLastStudent = function () {
-    const deleteButton = studentListElement.children[studentListElement.childElementCount - 1].querySelector('.student__delete');
-    deleteButton.addEventListener('click', deleteButtonClickHandler);
+  const deleteButton = studentListElement.children[studentListElement.childElementCount - 1].querySelector('.student__delete');
+  deleteButton.addEventListener('click', deleteButtonClickHandler);
 };
 
-const createStudentElement = function(studentName, faceNumber) {
-    const studentElement = document.createElement('article');
-    studentElement.classList.add('student');
+const createStudentElement = function (studentName, faceNumber) {
+  const studentElement = document.createElement('article');
+  studentElement.classList.add('student');
 
-    const faceElement = document.createElement('div');
-    faceElement.classList.add('student__face');
-    faceElement.classList.add('student__face_number_' + faceNumber);
-    studentElement.appendChild(faceElement);
-    
-    const nameElement = document.createElement('div');
-    nameElement.classList.add('student__name');
-    nameElement.innerText = studentName;
-    studentElement.appendChild(nameElement);
-    
-    const dateElement = document.createElement('div');
-    dateElement.classList.add('student__date');
-    dateElement.innerText = Date();
-    studentElement.appendChild(dateElement);
-    
-    const crossElement = document.createElement('div');
-    crossElement.classList.add('student__delete');
-    crossElement.innerText = 'x';
-    studentElement.appendChild(crossElement);
+  const faceElement = document.createElement('div');
+  faceElement.classList.add('student__face');
+  faceElement.classList.add('student__face_number_' + faceNumber);
+  studentElement.appendChild(faceElement);
 
-    return studentElement;
+  const nameElement = document.createElement('div');
+  nameElement.classList.add('student__name');
+  nameElement.innerText = studentName;
+  studentElement.appendChild(nameElement);
+
+  const dateElement = document.createElement('div');
+  dateElement.classList.add('student__date');
+  dateElement.innerText = Date();
+  studentElement.appendChild(dateElement);
+
+  const crossElement = document.createElement('div');
+  crossElement.classList.add('student__delete');
+  crossElement.innerText = 'x';
+  studentElement.appendChild(crossElement);
+
+  return studentElement;
 };
 
-const createStudentElementFromTemplate = function(studentName, faceNumber) {
-    const studentTemplate = document.querySelector('#student-template').content;
+const createStudentElementFromTemplate = function (studentName, faceNumber) {
+  const studentTemplate = document.querySelector('#student-template').content;
 
-    const studentElement = studentTemplate.cloneNode(true);
-    studentElement.querySelector('.student__face').classList.add('student__face_number_' + faceNumber);
-    studentElement.querySelector('.student__name').innerText = studentName;
-    studentElement.querySelector('.student__date').innerText = Date();
+  const studentElement = studentTemplate.cloneNode(true);
+  studentElement.querySelector('.student__face').classList.add('student__face_number_' + faceNumber);
+  studentElement.querySelector('.student__name').innerText = studentName;
+  studentElement.querySelector('.student__date').innerText = Date();
 
-    return studentElement;
+  return studentElement;
 };
 
 const insertWithInnerHtml = function (evt) {
-    const studentName = studentInput.value;
-    const faceNumber = Math.floor(Math.random() * 6);
+  const studentName = studentInput.value;
+  const faceNumber = Math.floor(Math.random() * 6);
 
-    studentListElement.innerHTML +=
-        `<article class="student">
+  studentListElement.innerHTML +=
+    `<article class="student">
             <div class="student__face student__face_number_${faceNumber}"></div>
             <div class="student__name">${studentName}</div>
             <div class="student__date">${Date()}</div>
             <div class="student__delete">x</div>
         </article>`;
 
-    addDeleteButtonClickHandlerToLastStudent();
+  addDeleteButtonClickHandlerToLastStudent();
 };
 
 const insertWithAdjacentHtml = function (evt) {
-    const studentName = studentInput.value;
-    const faceNumber = Math.floor(Math.random() * 6);
+  const studentName = studentInput.value;
+  const faceNumber = Math.floor(Math.random() * 6);
 
-    studentListElement.insertAdjacentHTML('beforeend', 
-        `<article class="student">
+  studentListElement.insertAdjacentHTML('beforeend',
+    `<article class="student">
             <div class="student__face student__face_number_${faceNumber}"></div>
             <div class="student__name">${studentName}</div>
             <div class="student__date">${Date()}</div>
             <div class="student__delete">x</div>
         </article>`);
 
-    addDeleteButtonClickHandlerToLastStudent();
+  addDeleteButtonClickHandlerToLastStudent();
 };
 
-const insertWithAppendChild = function(evt) {
-    const studentName = studentInput.value;
-    const faceNumber = Math.floor(Math.random() * 6);
+const insertWithAppendChild = function (evt) {
+  const studentName = studentInput.value;
+  const faceNumber = Math.floor(Math.random() * 6);
 
-    const studentElement = createStudentElement(studentName, faceNumber);
-    studentListElement.appendChild(studentElement);
+  const studentElement = createStudentElement(studentName, faceNumber);
+  studentListElement.appendChild(studentElement);
 
-    addDeleteButtonClickHandlerToLastStudent();
+  addDeleteButtonClickHandlerToLastStudent();
 };
 
-const insertWithAppend = function(evt) {
-    const studentName = studentInput.value;
-    const faceNumber = Math.floor(Math.random() * 6);
+const insertWithAppend = function (evt) {
+  const studentName = studentInput.value;
+  const faceNumber = Math.floor(Math.random() * 6);
 
-    const studentElement = createStudentElement(studentName, faceNumber);
-    studentListElement.append(studentElement);
+  const studentElement = createStudentElement(studentName, faceNumber);
+  studentListElement.append(studentElement);
 
-    addDeleteButtonClickHandlerToLastStudent();
+  addDeleteButtonClickHandlerToLastStudent();
 };
 
-const insertWithTemplate = function(evt) {
-    const studentName = studentInput.value;
-    const faceNumber = Math.floor(Math.random() * 6);
+const insertWithTemplate = function (evt) {
+  const studentName = studentInput.value;
+  const faceNumber = Math.floor(Math.random() * 6);
 
-    const studentElement = createStudentElementFromTemplate(studentName, faceNumber);
-    studentListElement.appendChild(studentElement);
+  const studentElement = createStudentElementFromTemplate(studentName, faceNumber);
+  studentListElement.appendChild(studentElement);
 
-    addDeleteButtonClickHandlerToLastStudent();
+  addDeleteButtonClickHandlerToLastStudent();
 };
 
 studentForm.addEventListener('submit', evt => evt.preventDefault());
